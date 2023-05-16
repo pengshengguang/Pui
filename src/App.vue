899c2882e014740f40cb9003f11d578abd89d7dd
彭胜光 <shengguang.peng@jdh.com.cn>
2023-05-16 15:33:51 +0800

    feat: 全局注册element-plus-icon 组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <el-button>123</el-button>
+    <el-icon-edit></el-icon-edit>
     <router-view></router-view>
   </div>
 </template>
@@ -8,5 +9,13 @@
 <script lang="ts" setup>
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+svg {
+  width: 1em;
+  height: 1em;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,17 @@ import App from './App.vue'
 import router from './router/index'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as Icons from '@element-plus/icons-vue'
+import { toLine } from './utils'
 
 const app = createApp(App)
+
+for (let i in Icons) {
+  // 注册全局组件
+  // console.log((Icons as any)[i])
+  app.component(`el-icon-${toLine(i)}`, (Icons as any)[i])
+}
+
 app
   .use(router)
   .use(ElementPlus)
