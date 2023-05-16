bed2f664f2bb1f481a1ad64a4da8027534959e17
彭胜光 <shengguang.peng@jdh.com.cn>
2023-05-16 14:51:05 +0800

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <el-button>123</el-button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
